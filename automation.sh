69241ae6cbeaba9c43e99d1e31780a31ba865b1d
myselfparmod <110894478+myselfparmod@users.noreply.github.com>
2022-08-31 22:56:23 +0530

    automation without cron

--- automation.sh
+++ automation.sh
@@ -32,18 +32,3 @@ then
     aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 fi
 
-docroot="/var/www/html"
-if [ ! -f ${docroot}/inventory.html ];
-then
-    echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' >{doc}/inventory.html
-fi
-if [[ -f ${docroot}/inventory.html ]];
-then
-    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
-	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/invetntory.html
-fi
-if [[ ! -f /etc/cron.d/automation ]];
-then
-    echo " * * * * * root/C1_course_assignment/automation.sh" >> /etc/cron.d/automation
-fi
-		
